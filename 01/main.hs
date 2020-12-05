2c167cdcd373f1ba1ecd13e72b9a9c4180cd2063
Alex Miller <alexmiller@gmail.com>
2020-12-05 14:54:56 -0800

    day 1, part 2

--- 01/main.hs
+++ 01/main.hs
@@ -4,9 +4,9 @@ main = do
   -- parse numbers into a list
   let nums = map readInt $ words contents
   -- find pair of numbers that adds to 2020
-  let pair = head [ (x, y) | x <- nums, y <- nums, x + y == 2020 ]
+  let triple = head [ [x, y, z] | x <- nums, y <- nums, z <- nums, x + y + z == 2020 ]
   -- multiply the pair and print
-  print $ uncurry (*) pair
+  print $ product triple
 
 readInt :: String -> Int
 readInt = read
