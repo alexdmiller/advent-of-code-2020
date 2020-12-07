a297d42e39dcbf940dcdb2aeece823f465c4fd4f
Alex Miller <alexmiller@gmail.com>
2020-12-07 07:23:37 -0800

    day 3, part 2

--- 03/part2.hs
+++ 03/part2.hs
@@ -1,7 +1,7 @@
 type Point = (Int, Int)
 
-step :: [String] -> Point -> Point -> Int -> Int
-step rows (slope_x, slope_y) (pos_x, pos_y) trees =
+step :: [String] -> Point -> Int -> Point -> Int
+step rows (pos_x, pos_y) trees (slope_x, slope_y) =
   let height = length rows
       width = length $ head rows
       current = rows !! pos_y !! pos_x
@@ -10,8 +10,9 @@ step rows (slope_x, slope_y) (pos_x, pos_y) trees =
         (pos_x + slope_x) `mod` width,
         pos_y + slope_y) in
     if next_y >= height then next_trees
-    else step rows (slope_x, slope_y) (next_x, next_y) next_trees
+    else step rows (next_x, next_y) next_trees (slope_x, slope_y)
 
 main = do
   contents <- readFile "input.txt"
-  print (step (lines contents) (3, 1) (0, 0) 0)
+  let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)] in
+    print . product $ map (step (lines contents) (0, 0) 0) slopes
